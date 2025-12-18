5c5f2470434cdf1a630247c1101f29fc2d087a37
shreevishnum2024eee-collab <shreevishnu.m2024eee@sece.ac.in>
2025-12-18 11:04:43 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'http://3.7.69.109:5001/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
